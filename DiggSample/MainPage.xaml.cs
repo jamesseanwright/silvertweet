238d3b5e0b0f95e210a759450494e72eaee3b7d2
James Wright <james.wright@bskyb.com>
2013-01-23 06:38:29 -0800

    SilverTweet rewrite with Model View ViewModel pattern

--- DiggSample/MainPage.xaml.cs
+++ DiggSample/MainPage.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Json;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
@@ -24,42 +17,6 @@ namespace SilverTweet
         {
             InitializeComponent();
             DataContext = new MainPageViewModel(); 
-        }
-
-        private void BtnSearch_Click(object sender, RoutedEventArgs e)
-        {
-            string twitterUrl = String.Format("http://search.twitter.com/search.json?q={0}", TxtSearch.Text);
-            WebClient twitterService = new WebClient();
-            twitterService.DownloadStringCompleted += new DownloadStringCompletedEventHandler(TwitterService_DownloadStoriesCompleted);
-            twitterService.DownloadStringAsync(new Uri(twitterUrl));
-        }
-
-        private void TwitterService_DownloadStoriesCompleted(object sender, DownloadStringCompletedEventArgs e)
-        {
-            if (e.Error == null)
-            {
-                DisplayTweets(e.Result);
-            }
-        }
-
-        private void DisplayTweets(string data)
-        {
-            JsonObject json = (JsonObject) JsonValue.Parse(data);
-            JsonArray results = (JsonArray) json["results"];
-
-            var tweets = from tweet in results
-                    select new Tweet
-                    {
-                        User = (string)tweet["from_user"],
-                        ProfileImage = (string)tweet["profile_image_url"],
-                        Text = (string)tweet["text"]
-                    };
-
-            var tweetObs = new ObservableCollection<Tweet>(tweets);
-
-            TweetList.SelectedIndex = -1;
-            this.DataContext = tweetObs;
-            
         }
     }
 }
--- DiggSample/MainPageViewModel.cs
+++ DiggSample/MainPageViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using MVVM;
 using System.IO;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Json;
 using System.Runtime.Serialization;
@@ -18,8 +20,55 @@ using System.Windows.Shapes;
 
 namespace SilverTweet
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
+        private string _query;
+
+        private ICommand _SearchCommand;
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return _SearchCommand;
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            _SearchCommand = new DelegateCommand<string>(null);
+        }
+
+        private void DownloadTweets(object sender, RoutedEventArgs e)
+        {
+            string twitterUrl = String.Format("http://search.twitter.com/search.json?q={0}", TxtSearch.Text);
+            WebClient twitterService = new WebClient();
+            twitterService.DownloadStringCompleted += new DownloadStringCompletedEventHandler(TwitterService_DownloadStoriesCompleted);
+            twitterService.DownloadStringAsync(new Uri(twitterUrl));
+        }
+
+        public void TwitterService_DownloadStoriesCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                CreateTweetsList(e.Result);
+            }
+        }
+
+        private void CreateTweetsList(string data)
+        {
+            JsonObject json = (JsonObject)JsonValue.Parse(data);
+            JsonArray results = (JsonArray)json["results"];
+
+            var tweets = from tweet in results
+                         select new Tweet
+                         {
+                             User = (string)tweet["from_user"],
+                             ProfileImage = (string)tweet["profile_image_url"],
+                             Text = (string)tweet["text"]
+                         };
+
+            var tweetObs = new ObservableCollection<Tweet>(tweets);
+        }
   
     }
 }
